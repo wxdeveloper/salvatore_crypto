a3a0ab7aafa814722f37401d93a0ee95a80d1f26
user123 <119897939+user5830213413@users.noreply.github.com>
2024-06-12 05:04:23 +0300

    [ADD] redis dependency for cryptorepository

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,22 +1,31 @@
 from fastapi import Depends
+from aioredis import Redis
 
 from repositories import CryptoRepository
 from services import CryptoService
 from models import Storage, CryptoLink
+from redis_CL import RedisCL
 
-async def get_storage():
+
+async def get_storage() -> Storage:
     return Storage()
 
-async def get_crypto_link():
+async def get_crypto_link() -> CryptoLink:
     return CryptoLink()
+
+async def get_redisCL(
+    redis: RedisCL
+) -> Redis:
+    return redis.client
 
 async def get_crypto_repository(
     link: CryptoLink = Depends(get_crypto_link), 
-    storage: Storage = Depends(get_storage)
-    ):
-    return CryptoRepository(link, storage)
+    storage: Storage = Depends(get_storage),
+    redis: RedisCL = Depends(get_redisCL)
+) -> CryptoRepository:
+    return CryptoRepository(link, storage, redis)
 
 async def get_crypto_service(
     repository: CryptoRepository = Depends(get_crypto_repository)
-    ):
+) -> CryptoService:
     return CryptoService(repository)    
