4e6ce4f237e62c441c430a206d77ff1eb58c8b18
user <wejsx@mail.ru>
2024-06-25 07:39:54 +0300

    add rpc api grpc links, endpoint get-services

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,9 +3,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from dependencies import get_crypto_service
+from dependencies import get_crypto_service, get_crypto_link
 from services import CryptoService
 from redis_CL import redis
+from models import CryptoLink
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -33,3 +34,16 @@ async def get_data(crypto_service: CryptoService = Depends(get_crypto_service)):
         return await crypto_service.get_data()
     except:
         HTTPException(status_code=403, detail='[Error external serve disconnect]')
+
+@app.get('/get-services', response_model=None)
+async def get_services(crypto_link: CryptoLink = Depends(get_crypto_link)):
+    response = [
+    {
+        'name': link.name, 
+        'rpc': link.link_rpc, 
+        'api': link.link_api, 
+        'grpc': link.link_grpc,
+    }
+    for link in crypto_link.array
+    ]
+    return response
--- backend/app/models.py
+++ backend/app/models.py
@@ -12,6 +12,9 @@ class Link:
     link_height: str
     link_total_balance: str
     link_refresh: str
+    link_rpc: str
+    link_api: str
+    link_grpc: str
 
 @dataclass
 class CryptoLink:
@@ -21,23 +24,36 @@ class CryptoLink:
             link_height='https://shentu-api.testovi.ch/cosmos/base/tendermint/v1beta1/blocks/latest',
             link_total_balance='https://shentu-api.testovi.ch/cosmos/staking/v1beta1/validators/shentuvaloper1m8qkqhmymvq5a8a6wfz35g523rx0pqamuuy7zt/delegations?height',
             link_refresh='https://wallet.keplr.app/chains/shentu?modal=validator&chain=shentu-2.2&validator_address=shentuvaloper1m8qkqhmymvq5a8a6wfz35g523rx0pqamuuy7zt',
+            link_rpc='1',
+            link_api='2',
+            link_grpc='3',
         ),
         Link(
             name='STAKE STARGAZE', 
             link_height='https://stargaze-api.testovi.ch/cosmos/base/tendermint/v1beta1/blocks/latest', 
             link_total_balance='https://stargaze-api.testovi.ch/cosmos/staking/v1beta1/validators/starsvaloper1tnx3z8ke9za65v32qxhu65mav2282jru8989a6/delegations?height',
-            link_refresh='https://wallet.keplr.app/chains/stargaze?modal=validator&chain=stargaze-1&validator_address=starsvaloper1tnx3z8ke9za65v32qxhu65mav2282jru8989a6'
+            link_refresh='https://wallet.keplr.app/chains/stargaze?modal=validator&chain=stargaze-1&validator_address=starsvaloper1tnx3z8ke9za65v32qxhu65mav2282jru8989a6',
+            link_rpc='https://stargaze-rpc.testovi.ch/',
+            link_api='https://stargaze-api.testovi.ch/',
+            link_grpc='https://stargaze-grpc.testovi.ch:9090',
+
         ),
         Link(
             name='STAKE COMDEX', 
             link_height='https://comdex-api.testovi.ch/cosmos/base/tendermint/v1beta1/blocks/latest', 
             link_total_balance='https://comdex-api.testovi.ch/cosmos/staking/v1beta1/validators/comdexvaloper1jum0pjcuxhj5dh7r6alhledh2ustw9wqrrftv8/delegations?height',
-            link_refresh='https://explorer.comdex.one/comdex-1/staking/comdexvaloper1jum0pjcuxhj5dh7r6alhledh2ustw9wqrrftv8'
+            link_refresh='https://explorer.comdex.one/comdex-1/staking/comdexvaloper1jum0pjcuxhj5dh7r6alhledh2ustw9wqrrftv8',
+            link_rpc='1',
+            link_api='2',
+            link_grpc='3',
         ),
         Link(
             name='STAKE JACKAL', 
             link_height='https://jackal-api.testovi.ch/cosmos/base/tendermint/v1beta1/blocks/latest', 
             link_total_balance='https://jackal-api.testovi.ch/cosmos/staking/v1beta1/validators/comdexvaloper1jum0pjcuxhj5dh7r6alhledh2ustw9wqrrftv8/delegations?height',
             link_refresh='https://ping.pub/jackal/staking/jklvaloper1wc0d9vrfrx5drsleuhdezlzyjw02yzap8che4w',
+            link_rpc='1',
+            link_api='2',
+            link_grpc='3',
         )
     ])
